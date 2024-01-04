2313ed3dac3dceeb7dd48d87126e11adb3183170
michael <michael.matve@gmail.com>
2024-01-05 00:30:47 +0200

    fix the solution2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lobby from "./Lobby";
 import CodePage from "./CodePage";
-
+/*
+This is the main app. It is responsible for routing the client to other pages.
+*/
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/CodePage/:id" element={<CodePage />} />
                 <Route path="/" element={<Lobby />} />
-                {/* Add other routes here as needed */}
             </Routes>
         </Router>
     );
