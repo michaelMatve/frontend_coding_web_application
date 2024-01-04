002247091f63b857309a8eeff9f1a3d1627447b1
michael <michael.matve@gmail.com>
2024-01-04 22:50:21 +0200

    try3

--- src/components/CodeList.js
+++ src/components/CodeList.js
@@ -42,13 +42,19 @@ class CodeList extends Component {
   
   render() {
     const { codesList } = this.state;
+  
+    // Check if codesList is null or undefined
+    if (codesList === null || codesList === undefined || codesList.length === 0) {
+      return null; // Don't render anything if codesList is not available or is empty
+    }
+  
     return (
       <div>
         <h1>od bdika</h1>
         <h2>{codesList}</h2>
         <ul>
           {codesList.map((code) => (
-            <ListCodeItem key={code._id} id={code._id} title= {code.title} />
+            <ListCodeItem key={code._id} id={code._id} title={code.title} />
           ))}
         </ul>
       </div>
